7463314c143c7fdebebd2fb6d2d399afcf7aa2ba
lanyanjun <yanjun.lan@riken.jp>
2021-06-04 15:43:21 +0900

    draft seurat script

---
+++ scripts/analysis1_seurat.R
@@ -0,0 +1,27 @@
+# Aim: This script will generate a single Seurat object for all scRNA-seq datasets and also a 
+# combined object for all scRNA-seq datasets 
+# Input: count file from the cellranger pipeline. 
+# Technical details: The scRNAs-seq datasets were generated using the 5' VDJ kit from 10x Genomics 
+# and fastq files were generated using cellranger. 
+
+# setting environment ------------------------------------------------------------------------------
+# loading libraries
+
+
+# load data 
+
+
+# preprocessing -----------------------------------------------------------------------------------
+
+
+# generation of Seurat object 
+
+
+# visualizations ----------------------------------------------------------------------------------
+
+
+
+
+
+
+
